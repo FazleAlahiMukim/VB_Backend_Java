f3db914bf3f2384514e5d52d3ddbec2affbe3359
Fazle Alahi <fazlealahi6@gmail.com>
2023-10-10 02:53:22 +0600

    fix

--- src/main/java/main/apiController/PlannerController.java
+++ src/main/java/main/apiController/PlannerController.java
@@ -677,13 +677,14 @@ public class PlannerController {
             //......................................end city added.....................................................
         }
         //destination_value created for plan 2..........................................................................
-
+        final int ultimate_cost = (int) totalCost;
+        final int ultimate_cost2 = (int) totalCost2;
         String type = "private";
         LinkedHashMap<String, Object> final_return_obj = new LinkedHashMap<>();
         LinkedHashMap<String, Object> final_return_obj2 = new LinkedHashMap<>();
         final_return_obj.put("planID", -1);
         final_return_obj.put("type", type);
-        final_return_obj.put("totalCost", (int) totalCost);
+        final_return_obj.put("totalCost", ultimate_cost);
         final_return_obj.put("planName", planName);
         final_return_obj.put("currentCity", currentCity);
         final_return_obj.put("destinations", destination_value);
@@ -691,7 +692,7 @@ public class PlannerController {
 
         final_return_obj2.put("planID", -2);
         final_return_obj2.put("type", type);
-        final_return_obj2.put("totalCost", (int) totalCost2);
+        final_return_obj2.put("totalCost", ultimate_cost2);
         final_return_obj2.put("planName", planName2);
         final_return_obj2.put("currentCity", currentCity);
         final_return_obj2.put("destinations", destination_value2);
